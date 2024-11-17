874220903e8c24d9bce6495566fbd0dda1fae6d8
Matias Alveal <matiasalvealc@gmail.com>
2024-11-16 21:01:40 -0300

    mi reserva y reservas

--- LasCabañitas/urls.py
+++ LasCabañitas/urls.py
@@ -21,5 +21,6 @@ from core import views
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('', views.home, name='home'),
-  #path('register/', views.register, name='register'),
+  path('miReserva/', views.miReserva, name='miReserva'),
+  path('reservas/', views.reservas, name='reserva'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, "core/home.html")
+
+def miReserva(request):
+  return render(request, "core/miReserva.html")
+
+def reservas(request):
+  return render(request, "core/reservas.html")
